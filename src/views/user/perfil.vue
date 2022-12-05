ea39e20bb4212fe95c7d5fdc1b15f489bc898100
RocioPinzon <pinzonbayonr@gmail.com>
2022-12-05 15:50:38 +0100

    GestTerr | Actualizacon

--- src/views/user/perfil.vue
+++ src/views/user/perfil.vue
@@ -2,75 +2,7 @@
   <Header/>
     <v-container class="">
       <v-row no-gutters>
-        <v-col
-            xs="12"
-            sm="12"
-            md="6"
-            lg="5"
-            xl="4">
-            <v-sheet class="pa-5 ma-2">
-                <v-card
-                        class="mx-auto"
-                        color="green"
-                        variant="outlined">
-                        <v-card-title class="d-flex justify-space-end"><v-icon>mdi-account</v-icon><h3>Datos perfil</h3></v-card-title>
-                        <v-card-text>
-                        <h3 class="py-3">Bienvenido/a</h3>
-                        <p>Nombre: {{ datosUser.name }}</p>
-                        <p>Apellidos: {{ datosUser.apellidos }}</p>
-                        <p>Username: {{ datosUser.username }}</p>
-                        <p>e-mail: {{ datosUser.email }}</p>
-                        </v-card-text>
-                    </v-card>
-            </v-sheet>
-        </v-col>
-        <v-col>
-          <v-sheet class="pa-5 ma-2">
-            <v-form
-              ref="form"
-              class="elevation-5 pa-6"
-              v-model="valid"
-              lazy-validation>
-              <h2 class="text-center pa-6">EDITAR MIS DATOS PERSONALES</h2>
-
-              <v-text-field
-                v-model="datosUser.name"
-                :counter="10"
-                :rules="nameRules"
-                label="Nombre"
-                variant="outlined"></v-text-field>
-
-              <v-text-field
-                v-model="datosUser.apellidos"
-                :counter="10"
-                :rules="nameRules"
-                label="Apellidos"
-                variant="outlined"></v-text-field>
-              <v-text-field
-                v-model="datosUser.username"
-                :counter="10"
-                :rules="nameRules"
-                label="Username"
-                variant="outlined"></v-text-field>
-                
-              <v-text-field
-                v-model="datosUser.email"
-                :rules="emailRules"
-                label="E-mail"
-                variant="outlined"
-                disabled
-                required></v-text-field>
-              <v-btn
-                color="success"
-                class="mr-4"
-                @click="actualizarDatosUser">
-                Actualizar</v-btn>
-
-            </v-form>
-          </v-sheet>
-        </v-col>
-  
-        <v-responsive width="100%"></v-responsive>
+       prueba
   
       </v-row>
     </v-container>
@@ -85,7 +17,7 @@
 
     export default {
       name: 'Perfil',
-      components: { Navigation,Header },
+      
         data: () => ({
           userId: null,
           datosUser:{}
